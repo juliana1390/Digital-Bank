6844a5a445b8ab71d9892193c05519f8ebc115a3
juliana1390 <juliana_santos@usp.br>
2022-05-29 17:46:26 -0300

    encapsulation

---
+++ src/CheckingAccount.java
@@ -0,0 +1,31 @@
+public class CheckingAccount {
+    private int branch;
+    private int number;
+    private double cashAtBank;
+
+
+    public class ToWithdrawTheMoney{
+
+    }
+
+    public class toMakeADeposit(){
+
+    }
+
+
+    public class ToWireTheMoney(){
+
+    }
+
+    public int getAgency() {
+        return branch;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public double getCashAtBank() {
+        return cashAtBank;
+    }
+}
---
+++ src/SavingsAccount.java
@@ -0,0 +1,31 @@
+public class SavingsAccount {
+    private int branch;
+    private int number;
+    private double cashAtBank;
+
+
+    public class ToWithdrawTheMoney{
+
+    }
+
+    public class toMakeADeposit(){
+
+    }
+
+
+    public class ToWireTheMoney(){
+
+    }
+
+    public int getBranch() {
+        return branch;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public double getCashAtBank() {
+        return cashAtBank;
+    }
+}
